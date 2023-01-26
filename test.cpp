016aef6bffa53a2b03781b2269f061ea6f007b2a
artemkharkovnyk <kharkovnykartem@gmail.com>
2023-01-26 17:15:05 +0200

    test halide code

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+
+
+TEST(SquareRootTest, NegativeNos) {
+    int a, b;
+    a = 2; b =3;
+    EXPECT_EQ(a, b);
+}
+ 
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+
